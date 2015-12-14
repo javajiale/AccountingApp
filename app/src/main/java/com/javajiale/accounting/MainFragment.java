917ebb1405d01532c3f37cdf2ce42a474a968b7c
javajiale <100683894@qq.com>
2015-12-14 19:58:36 +0800

    综合到了MAIN

--- app/src/main/java/com/javajiale/accounting/MainFragment.java
+++ app/src/main/java/com/javajiale/accounting/MainFragment.java
@@ -1,7 +1,10 @@
 package com.javajiale.accounting;
 
+import android.graphics.Color;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
@@ -9,11 +12,21 @@ import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+
+import java.util.ArrayList;
+
 /**
  * Created by javajiale on 2015/12/8.
  */
 public class MainFragment extends Fragment {
     private TextView text;
+    private PieChart mChart;
+
 
     @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -23,45 +36,166 @@ public class MainFragment extends Fragment {
 
     }
 
+
     @Override
     public void onActivityCreated(Bundle savedInstanceState){
         super.onActivityCreated(savedInstanceState);
         text= (TextView)getActivity().findViewById(R.id.xy_textView);
-        final ImageButton btn = (ImageButton)getActivity().findViewById(R.id.food_imageButton);
-        btn.setOnTouchListener(new View.OnTouchListener() {
-            int[] temp = new int[]{0, 0};
+        final ImageButton btn_food = (ImageButton)getActivity().findViewById(R.id.food_imageButton);
+        final ImageButton btn_shopping = (ImageButton)getActivity().findViewById(R.id.shopping_imageButton);
+        final ImageButton btn_car = (ImageButton)getActivity().findViewById(R.id.car_imageButton);
+        final ImageButton btn_live = (ImageButton)getActivity().findViewById(R.id.live_imageButton);
 
-            public boolean onTouch(View v, MotionEvent event) {
+        btn_food.setOnTouchListener(new tuoListener());
+        btn_shopping.setOnTouchListener(new tuoListener());
+        btn_live.setOnTouchListener(new tuoListener());
+        btn_car.setOnTouchListener(new tuoListener());
 
-                int eventaction = event.getAction();
+        mChart = (PieChart) getActivity().findViewById(R.id.spread_pie_chart);
+        PieData mPieData = getPieData(4, 100);
+        showChart(mChart, mPieData);
+    }
 
-                int x = (int) event.getRawX();
-                int y = (int) event.getRawY();
+    private class tuoListener implements View.OnTouchListener{
 
-                switch (eventaction) {
+        int[] temp = new int[]{0, 0};
 
-                    case MotionEvent.ACTION_DOWN: // touch down so check if the
-                        temp[0] = (int) event.getX();
-                        temp[1] = y - v.getTop();
-                        break;
+        public boolean onTouch(View v, MotionEvent event) {
 
-                    case MotionEvent.ACTION_MOVE: // touch drag with the ball
+            int eventaction = event.getAction();
 
-                        v.layout(x - temp[0], y - temp[1], x + v.getWidth()
-                                - temp[0], y - temp[1] + v.getHeight());
-                        break;
+            int x = (int) event.getRawX();
+            int y = (int) event.getRawY();
 
-                    case MotionEvent.ACTION_UP:
-                        text.setText(temp[0]+" "+temp[1]+"\n"+ (x + v.getWidth()
-                                - temp[0])+" "+( y - temp[1] + v.getHeight()));
-                        break;
-                }
+            switch (eventaction) {
 
-                return false;
+                case MotionEvent.ACTION_DOWN: // touch down so check if the
+                    temp[0] = (int) event.getX();
+                    temp[1] = y - v.getTop();
+                    break;
+
+                case MotionEvent.ACTION_MOVE: // touch drag with the ball
+
+                    v.layout(x - temp[0], y - temp[1], x + v.getWidth()
+                            - temp[0], y - temp[1] + v.getHeight());
+                    break;
+
+                case MotionEvent.ACTION_UP:
+                    text.setText(temp[0]+" "+temp[1]+"\n"+ (x + v.getWidth()
+                            - temp[0])+" "+( y - temp[1] + v.getHeight()));
+                    break;
             }
 
+            return false;
+        }
 
 
-        });
     }
+
+    private void showChart(PieChart pieChart, PieData pieData) {
+        pieChart.setHoleColorTransparent(true);
+
+        pieChart.setHoleRadius(45f);  //半径
+        pieChart.setTransparentCircleRadius(45f); // 半透明圈
+        //pieChart.setHoleRadius(0)  //实心圆
+
+        pieChart.setDescription("测试饼状图");
+
+        // mChart.setDrawYValues(true);
+        pieChart.setDrawCenterText(true);  //饼状图中间可以添加文字
+
+        pieChart.setDrawHoleEnabled(true);
+
+        pieChart.setRotationAngle(90); // 初始旋转角度
+
+        // draws the corresponding description value into the slice
+        // mChart.setDrawXValues(true);
+
+        // enable rotation of the chart by touch
+        pieChart.setRotationEnabled(true); // 可以手动旋转
+
+        // display percentage values
+        pieChart.setUsePercentValues(true);  //显示成百分比
+        // mChart.setUnit(" €");
+        // mChart.setDrawUnitsInChart(true);
+
+        // add a selection listener
+//      mChart.setOnChartValueSelectedListener(this);
+        // mChart.setTouchEnabled(false);
+
+//      mChart.setOnAnimationListener(this);
+
+        pieChart.setCenterText("Quarterly Revenue");  //饼状图中间的文字
+
+        //设置数据
+        pieChart.setData(pieData);
+
+        // undo all highlights
+//      pieChart.highlightValues(null);
+//      pieChart.invalidate();
+
+        Legend mLegend = pieChart.getLegend();  //设置比例图
+        mLegend.setPosition(Legend.LegendPosition.RIGHT_OF_CHART);  //最右边显示
+//      mLegend.setForm(LegendForm.LINE);  //设置比例图的形状，默认是方形
+        mLegend.setXEntrySpace(7f);
+        mLegend.setYEntrySpace(5f);
+
+        pieChart.animateXY(1000, 1000);  //设置动画
+        // mChart.spin(2000, 0, 360);
+    }
+
+    /**
+     *
+     * @param count 分成几部分
+     * @param range
+     */
+    private PieData getPieData(int count, float range) {
+
+        ArrayList<String> xValues = new ArrayList<String>();  //xVals用来表示每个饼块上的内容
+
+        for (int i = 0; i < count; i++) {
+            xValues.add("Quarterly" + (i + 1));  //饼块上显示成Quarterly1, Quarterly2, Quarterly3, Quarterly4
+        }
+
+        ArrayList<Entry> yValues = new ArrayList<Entry>();  //yVals用来表示封装每个饼块的实际数据
+
+        // 饼图数据
+        /**
+         * 将一个饼形图分成四部分， 四部分的数值比例为14:14:34:38
+         * 所以 14代表的百分比就是14%
+         */
+        float quarterly1 = 14;
+        float quarterly2 = 14;
+        float quarterly3 = 34;
+        float quarterly4 = 38;
+
+        yValues.add(new Entry(quarterly1, 0));
+        yValues.add(new Entry(quarterly2, 1));
+        yValues.add(new Entry(quarterly3, 2));
+        yValues.add(new Entry(quarterly4, 3));
+
+        //y轴的集合
+        PieDataSet pieDataSet = new PieDataSet(yValues, "Quarterly Revenue 2014"/*显示在比例图上*/);
+        pieDataSet.setSliceSpace(0f); //设置个饼状图之间的距离
+
+        ArrayList<Integer> colors = new ArrayList<Integer>();
+
+        // 饼图颜色
+        colors.add(Color.rgb(205, 205, 205));
+        colors.add(Color.rgb(114, 188, 223));
+        colors.add(Color.rgb(255, 123, 124));
+        colors.add(Color.rgb(57, 135, 200));
+
+        pieDataSet.setColors(colors);
+
+        DisplayMetrics metrics = getResources().getDisplayMetrics();
+        float px = 5 * (metrics.densityDpi / 160f);
+        pieDataSet.setSelectionShift(px); // 选中态多出的长度
+
+        PieData pieData = new PieData(xValues, pieDataSet);
+
+        return pieData;
+    }
+
+
 }
